6952171a47af7752a5bf05960a5e6fa051242c5b
lucarelyx <lucas.mosquerallosa@gmail.com>
2025-01-13 23:06:35 +0100

    Initial configuration and database.

---
+++ src/main/java/com/inditex/codemain/CodeMainApplication.java
@@ -0,0 +1,14 @@
+package com.inditex.codemain;
+
+import org.springframework.boot.SpringApplication;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeMainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodeMainApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/inditex/codemain/CodeMainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inditex.codemain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeMainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
