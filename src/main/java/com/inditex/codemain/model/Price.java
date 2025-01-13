c6e011ef30c7f6b4259e193ed6c7c044260f2296
lucarelyx <lucas.mosquerallosa@gmail.com>
2025-01-14 00:55:46 +0100

    Added getters and setters.

--- src/main/java/com/inditex/codemain/model/Price.java
+++ src/main/java/com/inditex/codemain/model/Price.java
@@ -1,9 +1,7 @@
 package com.inditex.codemain.model;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.time.LocalDateTime;
 
@@ -12,6 +10,8 @@ import java.time.LocalDateTime;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Getter
+@Setter
 public class Price {
 
     @Id
@@ -26,4 +26,6 @@ public class Price {
     private Integer priority;
     private Double price;
     private String curr;
+
+
 }
