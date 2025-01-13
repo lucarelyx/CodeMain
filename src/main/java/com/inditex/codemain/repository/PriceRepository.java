d926532b47d367defed76525cca56fd7d101056a
lucarelyx <lucas.mosquerallosa@gmail.com>
2025-01-13 23:27:12 +0100

    Repository Creation.

---
+++ src/main/java/com/inditex/codemain/repository/PriceRepository.java
@@ -0,0 +1,14 @@
+package com.inditex.codemain.repository;
+
+import com.inditex.codemain.model.Price;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public interface PriceRepository extends JpaRepository<Price, Long> {
+    @Query("SELECT p FROM Price p WHERE p.brandId = :brandId AND p.productId = :productId AND :applicationDate BETWEEN p.startDate AND p.endDate ORDER BY p.priority DESC")
+    List<Price> findApplicablePrices(@Param("brandId") Integer brandId, @Param("productId") Integer productId, @Param("applicationDate") LocalDateTime applicationDate);
+}
