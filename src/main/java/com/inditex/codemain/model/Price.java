4223a0d38e39b4a65349892a7782100c56fa69cc
lucarelyx <lucas.mosquerallosa@gmail.com>
2025-01-13 23:22:57 +0100

    Database model creation.

---
+++ src/main/java/com/inditex/codemain/model/Price.java
@@ -0,0 +1,29 @@
+package com.inditex.codemain.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "prices")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Price {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+
+    private Integer brandId;
+    private LocalDateTime startDate;
+    private LocalDateTime endDate;
+    private Integer priceList;
+    private Integer productId;
+    private Integer priority;
+    private Double price;
+    private String curr;
+}
