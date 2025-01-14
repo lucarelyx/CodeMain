7a7f10d165d79e66b01c8cbf05632e17ec6c558a
lucarelyx <lucas.mosquerallosa@gmail.com>
2025-01-14 01:36:05 +0100

    Global Exceptions Handler implemented.

---
+++ src/main/java/com/inditex/codemain/exception/GlobalExceptionHandler.java
@@ -0,0 +1,14 @@
+package com.inditex.codemain.exception;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.server.ResponseStatusException;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(ResponseStatusException.class)
+    public ResponseEntity<String> handleResponseStatusException(ResponseStatusException ex) {
+        return ResponseEntity.status(ex.getStatusCode()).body(ex.getReason());
+    }
+}
